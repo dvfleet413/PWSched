d5042913773ed157b7cddead67fa9e00e33d38fc
Dave Van Fleet <dvfleet413@gmail.com>
2020-01-14 17:16:07 -0500

    Added account creation email

--- app/mailers/application_mailer.rb
+++ app/mailers/application_mailer.rb
@@ -1,4 +1,4 @@
 class ApplicationMailer < ActionMailer::Base
-  default from: 'from@example.com'
+  default from: 'publicwitnessingscheduler@gmail.com'
   layout 'mailer'
 end
---
+++ app/mailers/shift_mailer.rb
@@ -0,0 +1,5 @@
+class ShiftMailer < ApplicationMailer
+  def shift_assigned_mail
+
+  end
+end
