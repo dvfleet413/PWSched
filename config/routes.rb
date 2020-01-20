d62e1f6c041f1d6f66b9a23192e4df8bd8c3fcb9
Dave Van Fleet <dvfleet413@gmail.com>
2020-01-19 22:51:03 -0500

    added custom pager for 404 and 500 errors

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,14 @@
 Rails.application.routes.draw do
+  get 'errors/not_found'
+  get 'errors/internal_server_error'
   get 'pages/help'
   devise_for :users, controllers: { registrations: 'users/registrations', passwords: 'users/passwords' }
   get 'welcome/index'
 
   resources :shifts
 
+  match "/404", :to => "errors#not_found", :via => :all
+  match "/500", :to => "errors#internal_server_error", :via => :all
 
   root 'welcome#index'
 end
